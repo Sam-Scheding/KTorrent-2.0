6ba11c78eeb900fb30a7ed04083b59b50e60482e
samscheding@gmail.com <samscheding@gmail.com>
2018-04-09 15:08:26 +1000

    Fixed an issue with build files not being included

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from cx_Freeze import setup, Executable
 
+includefiles = ['session.json', 'default.json', 'settings.json']
+
 setup(name="KTorrent",
       version="0.1",
       description="",
+      options={'build_exe': {'include_files':includefiles}},
       executables=[Executable("KTorrent.py")],
 )
